a8816256b38312676220c23f4a700fcdf699706e
Giorgio Riva <giorgio.riva@unimib.it>
2025-11-14 17:15:35 +0100

    Fix customized visualization patch

--- block_people.php
+++ block_people.php
@@ -57,6 +57,14 @@ class block_people extends block_base {
     }
 
     /**
+     * instance_allow_config function
+     * @return boolean
+     */
+    public function instance_allow_config() {
+        return true;
+    }
+
+    /**
      * instance_allow_multiple function
      * @return bool
      */
@@ -103,18 +111,20 @@ class block_people extends block_base {
         // Get context.
         $currentcontext = $this->page->context;
 
+        require_once(__DIR__ . '/locallib.php');
+
+        // Get roles from instance settings or default settings.
+        $rolesvisualization = block_people_get_roles_visualization($this, $currentcontext);
+        $roles = block_people_get_roles_to_be_shown($this, $rolesvisualization);
         // Get teachers separated by roles.
-        $roles = get_config('block_people', 'roles');
+        $teachers = [];
         if (!empty($roles)) {
-            $teacherroles = explode(',', $roles);
-            $teachers = get_role_users(
-                $teacherroles,
-                $currentcontext,
-                true,
-                'ra.id AS raid, r.id AS roleid, r.sortorder, u.id, u.lastname, u.firstname, u.firstnamephonetic,
+            $teachers = get_role_users($roles,
+                    $currentcontext,
+                    true,
+                    'ra.id AS raid, r.id AS roleid, r.sortorder, u.id, u.lastname, u.firstname, u.firstnamephonetic,
                             u.lastnamephonetic, u.middlename, u.alternatename, u.picture, u.imagealt, u.email, u.suspended',
-                'r.sortorder ASC, u.lastname ASC, u.firstname ASC'
-            );
+                    'r.sortorder ASC, u.lastname ASC, u.firstname ASC');
         } else {
             $teachers = [];
         }
@@ -190,15 +200,11 @@ class block_people extends block_base {
             // Teacher image.
             $this->content->text .= html_writer::start_tag('div', ['class' => 'image']);
             if (get_config('block_people', 'linkavatar') == 1 && has_capability('moodle/user:viewdetails', $currentcontext)) {
-                $this->content->text .= $OUTPUT->user_picture(
-                    $user,
-                    ['size' => 35, 'link' => true, 'courseid' => $COURSE->id, 'includefullname' => false]
-                );
+                $this->content->text .= $OUTPUT->user_picture($user,
+                        ['size' => 35, 'link' => true, 'courseid' => $COURSE->id, 'includefullname' => false]);
             } else {
-                $this->content->text .= $OUTPUT->user_picture(
-                    $user,
-                    ['size' => 35, 'link' => false, 'courseid' => $COURSE->id, 'includefullname' => false]
-                );
+                $this->content->text .= $OUTPUT->user_picture($user,
+                        ['size' => 35, 'link' => false, 'courseid' => $COURSE->id, 'includefullname' => false]);
             }
             $this->content->text .= html_writer::end_tag('div');
 
@@ -213,26 +219,14 @@ class block_people extends block_base {
             }
             $this->content->text .= html_writer::end_tag('div');
             $this->content->text .= html_writer::start_tag('div', ['class' => 'icons']);
-            if (
-                get_config('block_people', 'linkmessaging') == 1 &&
+            if (get_config('block_people', 'linkmessaging') == 1 &&
                     $CFG->messaging && has_capability('moodle/site:sendmessage', $currentcontext) && $teacher->id != $USER->id &&
-                    \core_message\api::can_send_message($teacher->id, $USER->id)
-            ) {
-                $this->content->text .= html_writer::start_tag(
-                    'a',
-                    ['href' => new \core\url('/message/index.php', ['id' => $teacher->id]),
-                    'title' => get_string(
-                        'sendmessageto',
-                        'core_message',
-                        fullname($teacher)
-                    ),
-                    ]
-                );
-                $this->content->text .= $OUTPUT->pix_icon(
-                    't/message',
-                    get_string('sendmessageto', 'core_message', fullname($teacher)),
-                    'moodle'
-                );
+                    \core_message\api::can_send_message($teacher->id, $USER->id)) {
+                $this->content->text .= html_writer::start_tag('a',
+                        ['href' => new \core\url('/message/index.php', ['id' => $teacher->id]),
+                              'title' => get_string('sendmessageto', 'core_message', fullname($teacher)), ]);
+                $this->content->text .= $OUTPUT->pix_icon('t/message',
+                        get_string('sendmessageto', 'core_message', fullname($teacher)), 'moodle');
                 $this->content->text .= html_writer::end_tag('a');
             }
             $this->content->text .= html_writer::end_tag('div');
@@ -240,6 +234,7 @@ class block_people extends block_base {
 
             // End output teacher.
             $this->content->text .= html_writer::end_tag('li');
+
         }
 
         // End role list if necessary.
@@ -257,17 +252,11 @@ class block_people extends block_base {
 
             // Only if user is allow to see participants list.
             if (course_can_view_participants($currentcontext)) {
-                $this->content->text .= html_writer::start_tag(
-                    'a',
+                $this->content->text .= html_writer::start_tag('a',
                     ['href' => new \core\url('/user/index.php', ['contextid' => $currentcontext->id]),
-                    'title' => get_string('participants'),
-                    ]
-                );
-                $this->content->text .= $OUTPUT->pix_icon(
-                    'i/users',
-                    get_string('participants', 'core'),
-                    'moodle'
-                );
+                          'title' => get_string('participants'), ]);
+                $this->content->text .= $OUTPUT->pix_icon('i/users',
+                        get_string('participants', 'core'), 'moodle');
                 $this->content->text .= get_string('participantslist', 'block_people');
                 $this->content->text .= html_writer::end_tag('a');
             } else {
--- lang/en/block_people.php
+++ lang/en/block_people.php
@@ -28,6 +28,7 @@ $string['noparticipantslist'] = 'Viewing the participants list is prohibited in 
 $string['participantslist'] = 'Show participants list';
 $string['people:addinstance'] = 'Add a new people block';
 $string['people:myaddinstance'] = 'Add a new people block to Dashboard';
+$string['people:overrideglobalsettings'] = 'Override global settings';
 $string['pluginname'] = 'People';
 $string['privacy:metadata'] = 'The people plugin provides extended functionality to Moodle users, but does not store any personal data.';
 $string['resetvisibilityerror'] = 'Oops... Something went wrong updating the database tables... <br/> The setting "Reset visibility" has been reset.';
@@ -56,3 +57,11 @@ After saving this option, the database operations for resetting the visibility w
 $string['setting_roles'] = 'Show these roles';
 $string['setting_roles_desc'] = 'This setting allows you to control which users appear in this block. Users need to have at least one of these roles in a course to be shown in the block.';
 $string['setting_rolesheading'] = 'Roles';
+$string['setting_overridableroles'] = 'Available roles for instance override';
+$string['setting_overridableroles_help'] = 'List of available roles in block instance\'s settings when local override is allowed.';
+
+$string['setting_overridedefaultroles'] = 'Override default roles';
+$string['setting_overridedefaultroles_help'] = 'List of available roles in block instance\'s settings when local override is allowed.';
+$string['setting_instanceroles'] = 'Roles to display';
+$string['setting_instanceroles_help'] = 'List of available roles in block instance\'s settings when local override is allowed and selected.';
+$string['setting_overridenotallowed'] = 'Instance override of default settings is not allowed';
